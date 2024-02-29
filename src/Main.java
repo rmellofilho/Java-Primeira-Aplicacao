9445160a5e8e5dc4b50872d658ad11944258f9c4
Robeto Melo <rmello.filho@hotmail.com>
2024-02-29 14:46:36 -0300

    classificacao

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
                         """ + anoDeLancamento;
                 System.out.println(sinopse);
 
-                int classificacao = (int) (media/2)
-                System.out.priintln(classificacao);
+                int classificacao = (int) (media/2);
+                System.out.println(classificacao);
     }
 }
